1fc20e1af4e485887e289aaeab247d3528d02bf5
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-26 16:36:35 +0100

    add some comments

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -1,22 +1,22 @@
 import './App.css'
 import { tasks } from './tasks'
 function App() {
-  let notCompletedCount=0
-  const notCompleted = tasks.filter((el)=>el.state !='completed')
-  notCompleted.forEach(()=>{notCompletedCount++})
-  function counterFunction(){
+  let notCompletedCount=0 //creo una var 
+  const notCompleted = tasks.filter((el)=>el.state !='completed') //filtro l'array, facenbdomi restituire i valori che mi interessano
+  notCompleted.forEach(()=>{notCompletedCount++}) //per ogni elemento incremento il contatore
+  function counterFunction(){ //creo una funzione che mi incrementa il contatore per vedere quanti sono i valori che ho filtrato dalla map 
     let count = 0;
     tasks.filter((el)=>el.state =='completed').forEach(()=>count++)
     return count
   }
-  return (
+  return ( //restituisco il jsx
     <div>
-      <h1 className='bgcolor-aqua'>Task Manager</h1>
+      <h1 className='bgcolor-aqua'>Task Manager</h1> 
       <div>
-        <h3>Current Task {notCompletedCount}</h3>
+         <h3>Current Task {notCompletedCount}</h3> {/*qui vado a restituire il valore del primo count */}
       </div>
       <div>{
-         notCompleted.map((el)=>{
+         notCompleted.map((el)=>{  {/*qui per ogni elemento vado a restituire un jsx */}
           return (
             <div key = {el.id}>
               <p>
