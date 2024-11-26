6a51aaba1530d15c51f6e1c410351b3ba67066e5
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-26 15:34:50 +0100

    add method to print on screen

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -1,11 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { tasks } from './tasks'
 function App() {
+  let notCompletedCount=0
+  const notCompleted = tasks.filter((el)=>el.state !='completed')
+  notCompleted.forEach(()=>{notCompletedCount++})
+
+  //let completedCount = 0;
+  //const completed = tasks.filter((el)=>el.state =='completed')
+  //completed.forEach(()=>{completedCount++})
+
+
   return (
-    
+    <div>
+      <h1>Task Manager</h1>
+      <div>
+        <h3>Current Task {notCompletedCount}</h3>
+      </div>
+      <div>{
+        notCompleted.map((el)=>{
+          return (
+            <div key = {el.id}>
+              <p>
+                <span>{el.title}</span>
+                <span>{el.state}</span>
+              </p>
+              <p>Priority {el.priority}</p>
+              <p>Est. Time {el.estimatedTime}</p>
+            </div>
+          )
+        }) 
+      }
+      </div>
+    </div>
   )
 }
 
